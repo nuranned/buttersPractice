890f11c27f2c9d120f45bf59be0651186266a635
nuranned <nuranned@gmail.com>
2017-07-27 11:13:48 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create({title: "BabyButter 4oz", description: "We have created our own line of baby butter featuring ethically sourced ingredients. This 98% organic moisturizer is free of preservative agents, parabens, and phthalates. No fragrance added.", price: 14.95})
+
+Item.create({title: "BabyButter 2oz", description: "We have created our own line of baby butter featuring ethically sourced ingredients. This 98% organic moisturizer is free of preservative agents, parabens, and phthalates. No fragrance added.", price: 7.95})
