ac7240a048064b618b5fcc8b75b5b4ac55f66a59
nuranned <nuranned@gmail.com>
2017-07-27 09:58:09 -0700

    added Stripe controller functionality and route

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,25 @@
 class ChargesController < ApplicationController
+  def new
+  end
+
+  def create
+    #amount in cents
+    @amount = 1000
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      :description => 'Rails Stripe customer',
+      :currency => 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to_new_charge_path
+  end
 end
